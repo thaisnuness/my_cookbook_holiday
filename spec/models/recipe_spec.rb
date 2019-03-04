d9c20d9b2b28a81260eb8e67e4f596943ea30395
Thais Nunes <thais.vnunes59@gmail.com>
2019-03-04 09:58:07 -0300

    Visitante visualiza detalhes de uma receita

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column(:title).of_type(:string) }
+  it { is_expected.to have_db_column(:recipe_type).of_type(:string) }
+  it { is_expected.to have_db_column(:cuisine).of_type(:string) }
+  it { is_expected.to have_db_column(:difficulty).of_type(:string) }
+  it { is_expected.to have_db_column(:cook_time).of_type(:integer) }
+  it { is_expected.to have_db_column(:ingredients).of_type(:text) }
+  it { is_expected.to have_db_column(:cook_method).of_type(:text) }
 end
